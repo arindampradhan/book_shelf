dca3f4aaed2cf42265ed9990b91b15ccf0b555f1
arindampradhan <arindampradhan10@gmail.com>
2014-07-06 23:52:14 +0530

    adding a test  img folder

--- routes.py
+++ routes.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request,flash
-
+from glob import iglob
+from os.path import basename
 
 app = Flask(__name__)
 
@@ -7,8 +8,13 @@ app = Flask(__name__)
 def home():
   return render_template('index.html')
 
+@app.route('/')
+def list_images():
+	pngs = iglob('hold/temp/*.png')
+	pngs = (basename(png) for pnk in  pngs	)
+	return render_template('list_images.html',pngs=pngs)
+	
 @app.route('/book')
 def about():
   return render_template('book.html')
 
-  
